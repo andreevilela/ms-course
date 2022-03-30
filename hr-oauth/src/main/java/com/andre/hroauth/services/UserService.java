1f538aff22bf048141bbeffacbb82b306ea04795
andreevilela <andrevibegoogle@hotmail.com>
2022-03-30 12:47:36 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/andre/hroauth/services/UserService.java
@@ -0,0 +1,27 @@
+package com.andre.hroauth.services;
+
+import com.andre.hroauth.entities.User;
+import com.andre.hroauth.feignclients.UserFeignClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    public User findByEmail(String email) throws IllegalAccessException {
+        User user = userFeignClient.findByEmail(email).getBody();
+        if (user == null) {
+            logger.error("Email not found: " + email);
+            throw new IllegalAccessException("Email not found");
+        }
+        logger.info("Email found: " + email);
+        return user;
+    }
+}
